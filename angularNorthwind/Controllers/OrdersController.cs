8da69b9922b9ae14abdd2c9742aa1a15d38c7522
Michael Grant <aubie87@gmail.com>
2016-01-21 21:37:32 -0600

    Added new Order View Model to begin to manage Orders in the web api.

---
+++ angularNorthwind/Controllers/OrdersController.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web.Http;
+using System.Web.Http.Description;
+using angularNorthwind.Models;
+using angularNorthwind.ViewModel;
+
+namespace angularNorthwind.Controllers
+{
+    public class OrdersController : ApiController
+    {
+        private Northwind db = new Northwind();
+
+        // GET: api/Orders
+        [Route("api/orders")]
+        public async Task<List<OrderVM>> GetOrders()
+        {
+            List<OrderVM> vmList = new List<OrderVM>();
+            //var orders = await db.Orders.Where(x=>x.ShipCountry == "USA").ToListAsync();
+            var orders = await db.Orders.ToListAsync();
+            foreach (Order order in orders)
+            {
+                vmList.Add(order);
+            }
+
+            return vmList;
+        }
+
+        // GET: api/Orders/5
+        [Route("api/orders/{orderId}")]
+        public async Task<OrderVM> GetOrder(int orderId)
+        {
+            Order order = await db.Orders.FindAsync(orderId);
+            if (order == null)
+            {
+                throw new HttpResponseException(
+                    Request.CreateErrorResponse(
+                        HttpStatusCode.NotFound,
+                        "Order Id: " + orderId + " was not found."));
+            }
+
+            OrderVM vm = order;
+
+            return vm;
+        }
+
+        // PUT: api/Orders/5
+        [ResponseType(typeof(void))]
+        public async Task<IHttpActionResult> PutOrder(int id, Order order)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (id != order.OrderID)
+            {
+                return BadRequest();
+            }
+
+            db.Entry(order).State = EntityState.Modified;
+
+            try
+            {
+                await db.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!OrderExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return StatusCode(HttpStatusCode.NoContent);
+        }
+
+        // POST: api/Orders
+        [ResponseType(typeof(Order))]
+        public async Task<IHttpActionResult> PostOrder(Order order)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            db.Orders.Add(order);
+            await db.SaveChangesAsync();
+
+            return CreatedAtRoute("DefaultApi", new { id = order.OrderID }, order);
+        }
+
+        // DELETE: api/Orders/5
+        [ResponseType(typeof(Order))]
+        public async Task<IHttpActionResult> DeleteOrder(int id)
+        {
+            Order order = await db.Orders.FindAsync(id);
+            if (order == null)
+            {
+                return NotFound();
+            }
+
+            db.Orders.Remove(order);
+            await db.SaveChangesAsync();
+
+            return Ok(order);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private bool OrderExists(int id)
+        {
+            return db.Orders.Count(e => e.OrderID == id) > 0;
+        }
+    }
+}
---
+++ angularNorthwind/ViewModel/OrderVM.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using angularNorthwind.Models;
+
+namespace angularNorthwind.ViewModel
+{
+    public class OrderVM
+    {
+        public int OrderID { get; set; }
+
+        public string CustomerID { get; set; }
+
+        public int? EmployeeID { get; set; }
+
+        public DateTime? OrderDate { get; set; }
+
+        public DateTime? RequiredDate { get; set; }
+
+        public DateTime? ShippedDate { get; set; }
+
+        public int? ShipVia { get; set; }
+
+        public decimal? Freight { get; set; }
+
+        public string ShipName { get; set; }
+
+        public string ShipAddress { get; set; }
+
+        public string ShipCity { get; set; }
+
+        public string ShipRegion { get; set; }
+
+        public string ShipPostalCode { get; set; }
+
+        public string ShipCountry { get; set; }
+
+        public static implicit operator OrderVM(Order order)
+        {
+            return new OrderVM()
+            {
+                CustomerID = order.CustomerID,
+                EmployeeID = order.EmployeeID,
+                Freight = order.Freight,
+                OrderDate = order.OrderDate,
+                OrderID = order.OrderID,
+                RequiredDate = order.RequiredDate,
+                ShipAddress = order.ShipAddress,
+                ShipCity = order.ShipCity,
+                ShipCountry = order.ShipCountry,
+                ShipName = order.ShipName,
+                ShippedDate = order.ShippedDate,
+                ShipPostalCode = order.ShipPostalCode,
+                ShipRegion = order.ShipRegion,
+                ShipVia = order.ShipVia
+            };
+        }
+
+        //public virtual Customer Customer { get; set; }
+
+        //public virtual Employee Employee { get; set; }
+
+        //public virtual ICollection<Order_Detail> Order_Details { get; set; }
+
+        //public virtual Shipper Shipper { get; set; }
+    }
+}
